445511271bad9f7ae629588f046cc72653cc552b
Prajwal Athreya Jagadish <67504508+PrajwalAthreya07@users.noreply.github.com>
2021-05-21 16:00:32 +0530

    Delete notification_application.py

--- notification_application.py
+++
@@ -1,9 +0,0 @@
-#importing Notification from the py-notifier package
-from pynotifier import Notification
-
-#creating the notification
-Notification(
-    title='Stand up now!',
-    description='standing up is good',
-    duration=100
-).send()
