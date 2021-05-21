0f511a4e3082ffd0199176557e9df8c77ff11a91
Prajwal Athreya Jagadish <67504508+PrajwalAthreya07@users.noreply.github.com>
2021-05-21 16:00:51 +0530

    Delete notification_application.py

--- notification_application.py
+++
@@ -1,9 +0,0 @@
-#importing Notification from the py-notifier package
-from pynotifier import Notification
-
-#creating the notification
-Notification(
-    title='Stand up now!',
-    description='standing up every 10 mins once',
-    duration=600
-).send()
